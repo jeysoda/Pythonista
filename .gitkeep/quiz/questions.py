f589c2787c98cc7f5ba15da4a9ba46c6c67c0980
Josecito <jeysoda@gmail.com>
2019-05-09 10:04:09 -0600

    Update questions.py

--- .gitkeep/quiz/questions.py
+++ .gitkeep/quiz/questions.py
@@ -1,2 +1,4 @@
 user = input("What is your Name? ")
-print("Hello",user," I Have a questions for you?/n {}".format(answer = input("Do you want to answer it?)
+print("Hello",user," I Have a questions for you?")
+money = input("How much money do you have right now? |Enter exact amount. example: $5.25|: ")
+print("Wow!",user,"you have",money,"does that make you mad?... it should...")
